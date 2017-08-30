eba62274b1bd2e70fe2c031db8a079b55f1863ad
haoge <470368500@qq.com>
2017-08-30 16:54:37 +0800

    avoid cyclic restart

--- core/src/main/java/com/lzh/router/replugin/core/RouterBridgeActivity.java
+++ core/src/main/java/com/lzh/router/replugin/core/RouterBridgeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.lzh.nonview.router.Router;
 import com.lzh.nonview.router.extras.RouteBundleExtras;
+import com.lzh.nonview.router.route.IRoute;
 
 /**
  * 插件间跳转的中间桥接页面。当Router启动一个未被加载的插件的路由时。在加载插件成功后。将会跳转到此页面并恢复之前启动的路由。
@@ -16,15 +17,22 @@ public class RouterBridgeActivity extends Activity {
 
     Uri uri;
     RouteBundleExtras extras;
+    IPluginCallback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         uri = getIntent().getParcelableExtra("uri");
         extras = getIntent().getParcelableExtra("extras");
+        callback = RePluginRouteCallback.get().getCallback();
         if (uri != null) {
             // 通过传递过来的uri与其对应的extras。恢复之前插件未加载时的路由。
-            Router.resume(uri, extras).open(RouterBridgeActivity.this);
+            IRoute resume = Router.resume(uri, extras);
+            if (resume instanceof IRoute.EmptyRoute) {
+                // 当恢复的路由为EmptyRoute时。代表此为无效路由。不进行启动操作。
+                return;
+            }
+            resume.open(RouterBridgeActivity.this);
         }
         IPluginCallback callback = RePluginRouteCallback.get().getCallback();
         if (callback != null) {
@@ -32,5 +40,6 @@ public class RouterBridgeActivity extends Activity {
         }
         finish();
     }
+
 
 }
