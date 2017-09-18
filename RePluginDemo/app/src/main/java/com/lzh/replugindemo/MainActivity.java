f5549d5941cd8daf475fda3cf7c367bf2cfc4b49
haoge <470368500@qq.com>
2017-09-18 13:05:05 +0800

    update demo

--- RePluginDemo/app/src/main/java/com/lzh/replugindemo/MainActivity.java
+++ RePluginDemo/app/src/main/java/com/lzh/replugindemo/MainActivity.java
@@ -3,22 +3,10 @@ package com.lzh.replugindemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.lzh.baselib.RePluginCallback;
-import com.lzh.baselib.SerialData;
-import com.lzh.compiler.parceler.Parceler;
 import com.lzh.nonview.router.Router;
 import com.lzh.nonview.router.anno.RouterRule;
-import com.lzh.nonview.router.route.IActionRoute;
-import com.lzh.nonview.router.route.IActivityRoute;
-import com.lzh.nonview.router.route.IBaseRoute;
-import com.lzh.replugindemo.pojo.User;
-import com.qihoo360.replugin.RePlugin;
-
-import java.io.Serializable;
 
 // 因为指定了baseUrl。 所以这里会使用baseUrl做组合。
 @RouterRule("main")
@@ -32,19 +20,7 @@ public class MainActivity extends Activity {
     }
 
     public void onClick(View v) {
-        IBaseRoute baseRoute = Router.create(((TextView)v).getText().toString()).getBaseRoute();
-        baseRoute.addExtras(Parceler.createFactory(null)
-                .put("serialData", new SerialData().setName("haoge"))
-                .getBundle());
-        if (baseRoute instanceof IActivityRoute) {
-            ((IActivityRoute) baseRoute).setAnim(R.anim.anim_fade_in, R.anim.anim_fade_out);
-        } else if (baseRoute instanceof IActionRoute) {
-            ((IActionRoute) baseRoute).addExtras(Parceler.createFactory(null)
-                    .setForceConvert(true)
-                    .put("user", new User("haoge", 18))
-                    .getBundle());
-        }
-        baseRoute.open(this);
+        Router.create(((TextView)v).getText().toString()).open(this);
     }
 
     @Override
--- RePluginDemo/plugina/src/main/java/com/lzh/plugina/PluginA.java
+++ RePluginDemo/plugina/src/main/java/com/lzh/plugina/PluginA.java
@@ -2,16 +2,10 @@ package com.lzh.plugina;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.lzh.baselib.SerialData;
 import com.lzh.nonview.router.Router;
 import com.lzh.nonview.router.anno.RouterRule;
-import com.lzh.nonview.router.route.IActionRoute;
-import com.lzh.nonview.router.route.IActivityRoute;
-import com.lzh.nonview.router.route.IBaseRoute;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,26 +13,17 @@ import butterknife.OnClick;
 @RouterRule("main")
 public class PluginA extends Activity {
 
-    SerialData data;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plugin_a);
-        data = (SerialData) getIntent().getExtras().getSerializable("serialData");
-        System.out.println("data = " + data);
         ButterKnife.bind(this);
     }
 
     @OnClick({R.id.toHost, R.id.toLogin, R.id.triggerActionHost, R.id.triggerActionPluginA, R.id.triggerActionUserCenter})
     void click(Button v) {
         String url = v.getText().toString();
-
-        IBaseRoute baseRoute = Router.create(url).getBaseRoute();
-        if (baseRoute instanceof IActivityRoute) {
-            ((IActivityRoute) baseRoute).setAnim(R.anim.anim_fade_in, R.anim.anim_fade_out);
-        }
-        baseRoute.open(this);
+        Router.create(url).open(this);
     }
 
 }
